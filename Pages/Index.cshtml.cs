afe887884ee6ac7cdb1780012d2b6842efe86409
Kasó Ádám <kaso.adam@gmail.com>
2023-02-25 19:30:24 +0100

    2023.02.25

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ToDoList.Dto;
 using ToDoList.Model;
 
 namespace ToDoList.Pages
